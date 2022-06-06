da95d8788784f97c34da1c1b48f1bbcf11b47fa5
Kallaidor <signalup3000@yandex.ru>
2022-06-06 21:24:05 +0300

    Проверка квадрата

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
-﻿Console.WriteLine("введите число");
-string input = Console.ReadLine();
-int number = Convert.ToInt32(input);
-int result = number * number;
-Console.WriteLine(number + " в квадрате будет " + result);
+﻿Console.WriteLine("введите число 1");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+
+Console.WriteLine("введите число 2");
+string input2 = Console.ReadLine();
+int number2 = Convert.ToInt32(input2);
+
+int square = number1 * number1;
+
+if (square == number2)
+{
+Console.WriteLine("ДА");
+}
+
+else 
+{
+Console.WriteLine("Нет");
+}
+
+
+
